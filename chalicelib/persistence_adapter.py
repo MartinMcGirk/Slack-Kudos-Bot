ba739e417e51ea36aeec8d48b4d47e002061997f
Martin <martin@mcgirk.com>
2018-02-02 22:42:13 +0000

    Fix giving of multiple emojis at once

--- chalicelib/persistence_adapter.py
+++ chalicelib/persistence_adapter.py
@@ -3,20 +3,19 @@ from collections import Counter
 from datetime import datetime
 
 
-
 DYNAMODB = boto3.client('dynamodb')
 
 
 def add_points_to_user(slack_message):
-    now = datetime.now()
-    now.isoformat()
-
-    DYNAMODB.put_item(TableName='emoji_log', Item={
-        'sender': {'S': slack_message.sender},
-        'recipient': {'S': slack_message.recipient},
-        'datetime_given': {'S': str(now)},
-        'channel': {'S': slack_message.channel}
-    })
+    for _ in range(slack_message.count_emojis_in_message()):
+        now = datetime.now()
+        now.isoformat()
+        DYNAMODB.put_item(TableName='emoji_log', Item={
+            'sender': {'S': slack_message.sender},
+            'recipient': {'S': slack_message.recipient},
+            'datetime_given': {'S': str(now)},
+            'channel': {'S': slack_message.channel}
+        })
 
 
 def get_user_points():

f1aba68d8ce0fa84fa0d06d52414d62fc9c1bf7e
Martin <martin@mcgirk.com>
2018-02-04 18:45:43 +0000

    Add verification token checking to all requests

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import logging
 from chalice import Chalice
 
-from chalicelib.global_constants import EMOJI
+from chalicelib.global_constants import EMOJI, VERIFICATION_TOKEN
 from chalicelib.kudos_bot import handle_message
 
 app = Chalice(app_name='kudosbot')
@@ -14,5 +15,8 @@ def index():
 @app.route('/handle-message', methods=['POST'])
 def create_user():
     request = app.current_request.json_body
+    if request['token'] != VERIFICATION_TOKEN:
+        logging.warning("Verification Error")
+        return "Verification Error"
     response = handle_message(request)
     return response or "200 OK"
